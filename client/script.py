68afd264a7169b4e1a910548dc9f0686849a81f2
Vishnu VS <ma18btech11011@iith.ac.in>
2021-10-11 18:23:46 +0530

    Some edits

--- client/script.py
+++ client/script.py
@@ -9,6 +9,7 @@ if vid.isOpened() == False:
 frame_width = int(vid.get(3))
 frame_height = int(vid.get(4))
 while True:
+    print("In main loop")
     out = cv2.VideoWriter('outfile.avi',cv2.VideoWriter_fourcc(*'MJPG'),10,(frame_width,frame_height))
     for i in range(50):
         ret, frame = vid.read()
@@ -19,7 +20,10 @@ while True:
     vid.release()
     out.release()
     with open('outfile.avi', 'rb') as f:
+        print("File written")
+        print("Posting request")
         r = requests.post('http://candle01.cse.iith.ac.in:8000', files={'file': f})
         if "Walking" in r.text:
+            print("Walking is in the response")
             os.system("node index.js register_event --id UP32CE6780 --latitude 26.83 --longitude 80.06 --orientation 270 --metadata 'person fall down' --car_latitude 26.83 --car_longitude 80.06")
     time.sleep(5)
